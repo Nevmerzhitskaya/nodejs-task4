eb527b2c2d43eca1d5bfcf80412314328fdb5900
nevmerzhitskaya <lamara@tut.by>
2023-01-10 16:46:30 +0100

    feat: add constants for errors and status code

---
+++ src/app/constants.ts
@@ -0,0 +1,11 @@
+export const enum StatusCode {
+    OK = 200,
+    BAD_REQUEST = 400,
+    NOT_FOUND = 404,
+    CREATED = 201,
+    NO_CONTENT = 204
+}
+
+export const enum ErrorMessage {
+    USER_NOT_FOUND = "User not found",
+}

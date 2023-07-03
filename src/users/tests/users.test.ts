8d9ab779697c9a09d9ee547b066a8e4dd6c95ad9
nevmerzhitskaya <lamara@tut.by>
2023-07-04 00:12:14 +0200

    fix: add some validation and change tests

--- src/users/tests/users.test.ts
+++ src/users/tests/users.test.ts
@@ -4,9 +4,14 @@ import request from 'supertest';
 
 const path = '/api/users';
 
-let testUser = {id: undefined, username: "TEST", age: 18, hobbies: ["dance", "cooking"] };
+let testUser = {id: undefined, username: "aaaaa", age: 18, hobbies: ["dance", "cooking"] };
+let testUser2 = {username: "TEST2222", age: 20, hobbies: ["dance", "cooking"] };
+let testUser3 = {id: undefined, username: "", age: 18, hobbies: ["dance", "cooking"] };
+let testUser4 = {id: undefined, username: "TEST", age: 18 };
+let testUser5 = {id: undefined, username: "TEST", age: '', hobbies: ["dance", "cooking"] };
 
-describe('Scenario 1 - Get all users', () => {
+describe('Scenario 1 ', () => {
+
     it('should return empty object', async () => {
         const empty = {};
         const response = await request(server).get(path);
@@ -14,41 +19,6 @@ describe('Scenario 1 - Get all users', () => {
         expect(response.statusCode).toBe(StatusCode.OK);
         expect(response.body).toEqual(empty);
     })
-})
-
-describe('Scenario 2 - A new user is created', () => {
-
-    it('if no username should return error message', async () => {
-        testUser.username = undefined;            
-        const response = await request(server).post(path).send(testUser);
-        expect(response.statusCode).toBe(StatusCode.BAD_REQUEST);
-        expect(response.body).toEqual({"code":StatusCode.BAD_REQUEST,"message": ErrorMessage.ERROR_VALIDATION});
-        testUser.username = 'Victor';
-    })
-
-    it('if empty username should return error message', async () => {
-        testUser.username = '';            
-        const response = await request(server).post(path).send(testUser);
-        expect(response.statusCode).toBe(StatusCode.BAD_REQUEST);
-        expect(response.body).toEqual({"code":StatusCode.BAD_REQUEST,"message": ErrorMessage.ERROR_VALIDATION});
-        testUser.username = 'Victor';
-    })
-
-    it('if no age should return error message', async () => {
-        testUser.age = undefined;
-        const response = await request(server).post(path).send(JSON.stringify(testUser));
-        expect(response.statusCode).toBe(StatusCode.BAD_REQUEST);
-        expect(response.body).toEqual({"code":StatusCode.BAD_REQUEST,"message": ErrorMessage.ERROR_VALIDATION});
-        testUser.age = 55;
-    })
-
-    it('if no hobbies should return error message', async () => {
-        testUser.hobbies = undefined;
-        const response = await request(server).post(path).send(JSON.stringify(testUser));
-        expect(response.statusCode).toBe(StatusCode.BAD_REQUEST);
-        expect(response.body).toEqual({"code":StatusCode.BAD_REQUEST,"message": ErrorMessage.ERROR_VALIDATION});
-        testUser.hobbies = ["dance", "cooking"];
-    })    
 
     it('should return newly created user', async () => {
         const response = await request(server).post(path).send(testUser);
@@ -60,14 +30,105 @@ describe('Scenario 2 - A new user is created', () => {
         expect(response.body.hobbies).toEqual(testUser.hobbies);
         testUser.id = response.body.id;
     })
-})
-
-describe('Scenario 3 - get user by its id', () => {
+    
     it('should return user', async () => {
         const response = await request(server).get(path + '/' + testUser.id);
 
         expect(response.statusCode).toBe(StatusCode.OK);
         expect(response.body).toEqual(testUser);
+    })   
+    
+    it('should return updated user', async () => {
+        const response = await request(server).put(path + '/' + testUser.id).send(testUser2);
+
+        expect(response.statusCode).toBe(StatusCode.OK);
+        expect(response.body.id).not.toBe('');
+        expect(response.body.username).toEqual(testUser2.username);
+        expect(response.body.age).toEqual(testUser2.age);
+        expect(response.body.hobbies).toEqual(testUser2.hobbies);
+    })    
+    
+    it('should delete user', async () => {
+        const response = await request(server).delete(path + '/' + testUser.id);
+
+        expect(response.statusCode).toBe(StatusCode.NO_CONTENT);
+    })
+    
+    it('there is no such object', async () => {
+        const response = await request(server).get(path + '/' + testUser.id);
+
+        expect(response.statusCode).toBe(StatusCode.NOT_FOUND);
+    })   
+})
+
+describe('Scenario 2', () => {
+
+    it('should return empty object', async () => {
+        const empty = {};
+        const response = await request(server).get(path);
+
+        expect(response.statusCode).toBe(StatusCode.OK);
+        expect(response.body).toEqual(empty);
+    })
+
+    it('if empty username should return error message', async () => {          
+        const response = await request(server).post(path).send(testUser3);
+        expect(response.statusCode).toBe(StatusCode.BAD_REQUEST);
+        expect(response.body).toEqual({"code":StatusCode.BAD_REQUEST,"message": ErrorMessage.ERROR_VALIDATION});
+    })
+
+    it('if no age should return error message', async () => {
+        const response = await request(server).post(path).send(JSON.stringify(testUser5));
+        expect(response.statusCode).toBe(StatusCode.BAD_REQUEST);
+        expect(response.body).toEqual({"code":StatusCode.BAD_REQUEST,"message": ErrorMessage.ERROR_VALIDATION});
+    })
+
+    it('if no hobbies should return error message', async () => {
+        const response = await request(server).post(path).send(JSON.stringify(testUser4));
+        expect(response.statusCode).toBe(StatusCode.BAD_REQUEST);
+        expect(response.body).toEqual({"code":StatusCode.BAD_REQUEST,"message": ErrorMessage.ERROR_VALIDATION});
+    })    
+
+    it('should return newly created user', async () => {
+        const response = await request(server).post(path).send(testUser);
+        expect(response.statusCode).toBe(StatusCode.CREATED);
+        expect(response.body.id).not.toBe('');
+        expect(response.body.username).toEqual(testUser.username);
+        expect(response.body.age).toEqual(testUser.age);
+        expect(response.body.hobbies).toEqual(testUser.hobbies);
+        testUser.id = response.body.id;
+    })
+
+    it('should return newly created user', async () => {
+        const response = await request(server).post(path).send(testUser);
+        expect(response.statusCode).toBe(StatusCode.CREATED);
+        expect(response.body.id).not.toBe('');
+        expect(response.body.username).toEqual(testUser.username);
+        expect(response.body.age).toEqual(testUser.age);
+        expect(response.body.hobbies).toEqual(testUser.hobbies);
+        testUser.id = response.body.id;
+    })
+
+    it('should return number of user', async () => {
+        const response = await request(server).get(path);
+
+        expect(response.statusCode).toBe(StatusCode.OK);
+        expect(Object.keys(response.body).length).toEqual(2);
+    })
+
+})
+
+describe('Scenario 3 - get user by its id', () => {
+
+
+    it('should return newly created user', async () => {
+        const response = await request(server).post(path).send(testUser);
+        expect(response.statusCode).toBe(StatusCode.CREATED);
+        expect(response.body.id).not.toBe('');
+        expect(response.body.username).toEqual(testUser.username);
+        expect(response.body.age).toEqual(testUser.age);
+        expect(response.body.hobbies).toEqual(testUser.hobbies);
+        testUser.id = response.body.id;
     })
 
     it('should return error message that no user with this id', async () => {
@@ -83,4 +144,18 @@ describe('Scenario 3 - get user by its id', () => {
         expect(response.statusCode).toBe(StatusCode.BAD_REQUEST);
         expect(response.body).toEqual({"code":StatusCode.BAD_REQUEST,"message": ErrorMessage.INVALID_USER_ID});
     })
+    
+    it('should delete user', async () => {
+        const response = await request(server).delete(path + '/' + testUser.id);
+
+        expect(response.statusCode).toBe(StatusCode.NO_CONTENT);
+    })    
+
+    it('should return object', async () => {
+        const response = await request(server).get(path);
+
+        expect(response.statusCode).toBe(StatusCode.OK);
+        expect(response.body).toEqual(expect.any(Object));
+    })
+
 })
